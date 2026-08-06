e68f438c7c9f6e24ddc7d4e772ef7501f3676709
Chaitanya Takale <chaitanyatakle60@gmail.com>
2026-08-06 23:15:01 +0530

    implement Clerk authentication and environment variable management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
-import {ClerkProvider} from '@clerk/react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { ClerkProvider } from '@clerk/react';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider afterSignOutUrl="/">
-<App />
-</ClerkProvider>
+    <ClerkProvider>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
